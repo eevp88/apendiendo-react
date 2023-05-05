c2ee565be3a6168e039f50069f64e6c9be28c129
Enzo Vera <enzo.vera@west-ingenieria.cl>
2023-05-05 12:31:11 -0400

    third project

---
+++ projects/03_mouse-follower/src/App.jsx
@@ -0,0 +1,64 @@
+import { useEffect , useState} from "react"
+
+
+const FollowMouse =  () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position , setPosition] = useState({ x: 0, y: 0 })
+  useEffect(()=>{
+    console.log('efecto' , {enabled})
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+     // cleanup:
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias, antes de ejecutar
+    //    el efecto de nuevo
+    return () => { // cleanup method
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+  return (
+    <>
+    <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px,${position.y}px)`
+      }}
+      />
+    <button onClick={()=> setEnabled(!enabled)}>
+      {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+    </button>
+    </>
+  )
+
+}
+
+function App() {
+  const [mounted, setMounted] = useState(true)
+  return (
+    <>
+    <h3>Proyecto 3</h3>
+      {mounted && <FollowMouse />}
+      <button onClick={() => setMounted(!mounted)}>
+        Toggle mounted FolloweMouse component
+      </button>
+    </>
+  )
+}
+
+export default App
